d99591f5c77ad4b7d64bc35bbe5550e435bc76e0
Artur Michalak <arturmichalak96@gmail.com>
2021-03-24 13:20:26 +0100

    add pkb services

---
+++ PKB/PKBParserServices.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+using System;
+
+namespace PKB
+{
+    public static class PKBParserServices
+    {
+        public static void SetFollows(this PKBStore pkb, Statement s1, Statement s2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SetParent(this PKBStore pkb, Statement s1, Statement s2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PKB/PKBStore.cs
+++ PKB/PKBStore.cs
@@ -12,14 +12,16 @@ namespace PKB
 
         public static PKBStore Instance { get { return _instance; } }
 
-        public void SetFollows(Statement s1, Statement s2)
-        {
-            throw new System.NotImplementedException();
-        }
+        #region parser services
+        public void SetFollows(Statement s1, Statement s2) => _instance.SetFollows(s1, s2);
 
-        public void SetParent(Statement s1, Statement s2)
-        {
-            throw new System.NotImplementedException();
-        }
+        public void SetParent(Statement s1, Statement s2) => _instance.SetParent(s1, s2);
+
+        // TODO: ...
+        #endregion
+
+        #region pql services
+        // TODO: ...
+        #endregion
     }
 }
