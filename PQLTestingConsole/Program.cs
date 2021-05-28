7fb191aa41b44fd0c8d9aed37d5406237b2e3c46
kamilm758@gmail.com <kamilm758@gmail.com>
2021-05-28 16:56:51 +0200

    Merge

--- PQLTestingConsole/Program.cs
+++ PQLTestingConsole/Program.cs
@@ -9,10 +9,9 @@ namespace PQLTestingConsole
         static void Main(string[] args)
         {
             var queryPreprocessor = new QueryPreprocessor();
-            string query = "assign a,a1; stmt s,s1; variable v; procedure p; constant c; select p with v.varName=p.procName";
+            string query = "assign a,a1; stmt s,s1; variable v; procedure p; constant c; select p such that Modifies (a,v) with v.varName=p.procName";
             QueryTree queryTree = queryPreprocessor.ParseQuery(query);
             QueryEvaluator queryEvaluator = new QueryEvaluator();
-            List<object> queryResultsRaw = queryEvaluator.GetQueryResultsRaw(queryTree);
         }
     }
 }
