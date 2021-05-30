41e25e6cb8cf07f2007020633f79ea52d8b65500
kamilm758@gmail.com <kamilm758@gmail.com>
2021-05-30 19:56:19 +0200

    Dodano budowanie tabelki rezulatat

--- PQLTestingConsole/Program.cs
+++ PQLTestingConsole/Program.cs
@@ -9,7 +9,7 @@ namespace PQLTestingConsole
         static void Main(string[] args)
         {
             var queryPreprocessor = new QueryPreprocessor();
-            string query = "assign a,a1; stmt s,s1; variable v; procedure p; constant c; select p such that Modifies (a,v) with v.varName=p.procName";
+            string query = "assign a,a1; stmt s,s1; variable v; procedure p; constant c; select p such that Modifies (2,\"ff)\"";
             QueryTree queryTree = queryPreprocessor.ParseQuery(query);
             QueryEvaluator queryEvaluator = new QueryEvaluator();
         }
---
+++ QueryProcessor/ResultGeneration/ResultTable.cs
@@ -0,0 +1,23 @@
+﻿using QueryProcessor.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryProcessor.ResultGeneration
+{
+    public class ResultTable
+    {
+        private readonly Dictionary<string, RelationArgumentType> _declaredSymbols;
+        private readonly List<ResultTableRow> _resultTableRows = new List<ResultTableRow>();
+
+        public ResultTable(Dictionary<string, RelationArgumentType> declaredSymbols)
+        {
+            _declaredSymbols = declaredSymbols;
+        }
+
+        public void AddRelationResult(string firstArgumentName, string firstArgumentValue, string secondArgumentName, string secondArgumentValue)
+        {
+
+        }
+    }
+}
